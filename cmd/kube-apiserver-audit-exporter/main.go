9a22f1f6a08289953f2c8c3312a4f4bca30d40d4
Shiming Zhang <wzshiming@hotmail.com>
2025-03-14 16:11:00 +0800

    Add --delay

--- cmd/kube-apiserver-audit-exporter/main.go
+++ cmd/kube-apiserver-audit-exporter/main.go
@@ -4,6 +4,7 @@ import (
 	"log/slog"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/spf13/pflag"
 	"github.com/wzshiming/kube-apiserver-audit-exporter/exporter"
@@ -14,6 +15,7 @@ var (
 	address      = ":8080"
 	cluster      = ""
 	replay       = false
+	delay        time.Duration
 )
 
 func init() {
@@ -21,25 +23,31 @@ func init() {
 	pflag.StringVar(&address, "address", address, "Address to listen on")
 	pflag.StringVar(&cluster, "cluster-label", cluster, "Default cluster label of metrics")
 	pflag.BoolVar(&replay, "replay", replay, "replay the audit log")
+	pflag.DurationVar(&delay, "delay", 0, "delay to start")
 	pflag.Parse()
 }
 
 func main() {
-	for _, p := range auditLogPath {
-		ns := strings.SplitN(p, ":", 2)
-		path := ns[0]
-		clusterLabel := cluster
-		if len(ns) > 1 {
-			clusterLabel = ns[1]
+	go func() {
+		if delay > 0 {
+			time.Sleep(delay)
 		}
+		for _, p := range auditLogPath {
+			ns := strings.SplitN(p, ":", 2)
+			path := ns[0]
+			clusterLabel := cluster
+			if len(ns) > 1 {
+				clusterLabel = ns[1]
+			}
 
-		e := exporter.NewExporter(
-			exporter.WithReplay(replay),
-			exporter.WithFile(path),
-			exporter.WithClusterLabel(clusterLabel),
-		)
-		e.Start()
-	}
+			e := exporter.NewExporter(
+				exporter.WithReplay(replay),
+				exporter.WithFile(path),
+				exporter.WithClusterLabel(clusterLabel),
+			)
+			e.Start()
+		}
+	}()
 
 	if err := exporter.ListenAndServe(address); err != nil {
 		slog.Error("Failed", "err", err)
